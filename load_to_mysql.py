689fb4c545762d39e014efebce0ae2267897165d
Sachin Sharma <sachinsharma56432@gmail.com>
2026-05-19 11:08:33 +0530

    Update load_to_mysql.py

--- load_to_mysql.py
+++ load_to_mysql.py
@@ -3,46 +3,38 @@ import mysql.connector
 import os
 from dotenv import load_dotenv
 
-# 1️ Load environment variables
 load_dotenv()
 
-# 2️ Read MySQL credentials from .env
 DB_HOST = os.getenv("MYSQL_HOST")
 DB_USER = os.getenv("MYSQL_USER")
 DB_PASSWORD = os.getenv("MYSQL_PASSWORD")
 DB_NAME = os.getenv("MYSQL_DATABASE")
+DB_PORT = int(os.getenv("MYSQL_PORT", 3306))
 
-# 3️ Connect to MySQL
 conn = mysql.connector.connect(
     host=DB_HOST,
     user=DB_USER,
     password=DB_PASSWORD,
-    database=DB_NAME
+    database=DB_NAME,
+    port=DB_PORT
 )
 
 cursor = conn.cursor()
-
 print("Connected to MySQL")
 
-# 4️ Read cleaned CSV data
 df = pd.read_csv("weather_cleaned.csv")
 
-# 5️ SQL insert query
 insert_query = """
 INSERT INTO weather_data
 (city, country, temperature_c, humidity, wind_kph, condition_text, timestamp)
 VALUES (%s, %s, %s, %s, %s, %s, %s)
 """
 
-# 6️ Insert each row into MySQL
 for _, row in df.iterrows():
     cursor.execute(insert_query, tuple(row))
 
-# 7️ Commit changes
 conn.commit()
-
-# 8️ Close connection
 cursor.close()
 conn.close()
 
-print("STEP ✔ Data loaded into MySQL successfully")
+print("STEP 3 ✔ Data loaded into MySQL successfully")
